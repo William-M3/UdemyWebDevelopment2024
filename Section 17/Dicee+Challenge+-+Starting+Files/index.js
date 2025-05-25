f077cee256e880e0b54daf81770a54d4f43ec169
William-M3 <WilliamTruMurphy@gmail.com>
2025-05-24 22:44:24 -0400

    Dice Challenge Complete

---
+++ Section 17/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,21 @@
+
+
+var randomNumber1 = Math.ceil(Math.random() * 6);
+var dice1 = "./images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute('src', dice1);
+
+var randomNumber2 = Math.ceil(Math.random() * 6);
+var dice2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img2").setAttribute('src', dice2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!"
+}
+else {
+    document.querySelector("h1").textContent = "Draw!"
+}
